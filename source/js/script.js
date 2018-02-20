6b781e6fa245f9839e88eae7fc1da4f53f5976ae
Selivanov <superexpulsion@mail.ru>
2018-02-20 17:39:24 +0300

    исправил ошибки после первой проверки

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,16 @@
 ymaps.ready(init);
-  var myMap,
-    myPlacemark1;
-  function init(){
-    myMap = new ymaps.Map ("map", {
-      center: [34.86591303, -111.76364148],
-      zoom: 15
-    });
-  myPlacemark1 = new ymaps.Placemark([34.86591303, -111.763641484], {
-    balloonContentHeader: 'Аризона',
-    balloonContentBody: 'г. Седона',
-    balloonContentFooter: 'Приезжайте',
-    hintContent: 'Здесь самые лучшие места'});
-  myMap.geoObjects.add(myPlacemark1);
-  }
+var myMap,
+myPlacemark1;
+function init(){
+  myMap = new ymaps.Map ("map", {
+    center: [34.86591303, -111.76364148],
+    zoom: 15
+  });
+myPlacemark1 = new ymaps.Placemark([34.86591303, -111.763641484], {
+  balloonContentHeader: "Аризона",
+  balloonContentBody: "г. Седона",
+  balloonContentFooter: "риезжайте",
+  hintContent: "Здесь самые лучшие места"
+});
+myMap.geoObjects.add(myPlacemark1);
+}
